734df99effb36faac7c615bb39de05a1c12ac11e
Mahfuz <mahfuz225bd@gmail.com>
2024-12-11 16:24:25 +0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'plant-disease-detection',
+  webDir: 'dist'
+};
+
+export default config;
